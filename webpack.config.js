88aa70cefc3d216c98ba4b4c808dee7e8301c6c3
Matt Shaw <matthew.shaw@landregistry.gov.uk>
2025-03-23 23:48:34 +0000

    use babel and postcss presets

--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,37 @@
-"use strict";
-
-const path = require("path");
-const autoprefixer = require("autoprefixer");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const path = require("path");
+const postcssPresetEnv = require("postcss-preset-env");
 
 module.exports = {
   mode: "production",
   devtool: "source-map",
-  entry: ["./src/js/main.js", "./src/scss/styles.scss"],
+  devServer: {
+    static: path.resolve(__dirname, "dist"),
+  },
+  entry: ["./src/js/main.mjs", "./src/scss/main.scss"],
   output: {
     filename: "main.min.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
-  devServer: {
-    static: path.resolve(__dirname, "dist"),
-    port: 8080,
-    hot: true,
-  },
-  plugins: [
-    new HtmlWebpackPlugin({ template: "./src/index.html" }),
-    new MiniCssExtractPlugin({
-      filename: "main.min.css",
-    }),
-  ],
-  optimization: {
-    minimizer: [`...`, new CssMinimizerPlugin()],
-  },
   module: {
     rules: [
       {
-        test: /\.(scss)$/,
+        test: /\.scss$/,
         use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
           {
-            // Extracts CSS for each JS file that includes CSS
-            loader: MiniCssExtractPlugin.loader,
-          },
-          {
-            // Interprets `@import` and `url()` like `import/require()` and will resolve them
-            loader: "css-loader",
-          },
-          {
-            // Loader for webpack to process CSS with PostCSS
             loader: "postcss-loader",
             options: {
               postcssOptions: {
-                plugins: [autoprefixer],
+                plugins: [postcssPresetEnv],
               },
             },
           },
           {
-            // Loads a SASS/SCSS file and compiles it to CSS
             loader: "sass-loader",
             options: {
               sassOptions: {
@@ -63,27 +42,49 @@ module.exports = {
         ],
       },
       {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
         mimetype: "image/svg+xml",
         scheme: "data",
         type: "asset/resource",
         generator: {
-          filename: "icons/[hash].svg",
+          filename: "icons/[name][ext][query]",
         },
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
         generator: {
-          filename: "images/[hash][ext][query]",
+          filename: "images/[name][ext][query]",
         },
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: "asset/resource",
         generator: {
-          filename: "fonts/[hash][ext][query]",
+          filename: "fonts/[name][ext][query]",
         },
       },
     ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "main.min.css",
+    }),
+    new HtmlWebpackPlugin({ template: "./src/index.html" }),
+  ],
+  resolve: {
+    modules: [path.resolve(__dirname, "node_modules")],
+  },
+  optimization: {
+    minimizer: [`...`, new CssMinimizerPlugin()],
   },
 };
