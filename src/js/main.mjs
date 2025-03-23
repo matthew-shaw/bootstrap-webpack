9a7e20383f6142ca5f9c2321d3c01fd0e22cc8dd
Matt Shaw <matthew.shaw@landregistry.gov.uk>
2025-03-23 23:22:54 +0000

    fully qualified

--- src/js/main.mjs
+++ src/js/main.mjs
@@ -1,6 +1,6 @@
 // Import just what we need
 // import 'bootstrap/js/dist/alert';
-import "bootstrap/js/dist/button";
+import "bootstrap/js/dist/button.js";
 // import 'bootstrap/js/dist/carousel';
 // import 'bootstrap/js/dist/collapse';
 // import 'bootstrap/js/dist/dropdown';
